ecc03e084cc392bf0a4adb63b9cd8e89cb0acc1a
Nick Balestra <nick@balestra.ch>
2018-04-30 16:21:45 +0100

    moar tests

--- packages/ot-oc-template-react-email-compiler/__tests__/createExcludeRegex.test.js
+++ packages/ot-oc-template-react-email-compiler/__tests__/createExcludeRegex.test.js
@@ -3,6 +3,7 @@ const createExcludeRegex = require("../lib/to-abstract-base-template-utils/creat
 test("should create a regex that match against any node module set aside given ones", () => {
   const regex = createExcludeRegex([
     "ot-oc-template-react-email-compiler/utils",
+    "@ot-react-ui/timeslots",
     "underscore"
   ]);
   expect(regex).toBeInstanceOf(RegExp);
@@ -11,9 +12,11 @@ test("should create a regex that match against any node module set aside given o
   expect(regex.test("node_modules/ot-oc-template-react-email-compiler")).toBe(
     true
   );
+  expect(regex.test("node_modules/@ot-react-ui/dtp")).toBe(true);
 
   expect(
     regex.test("node_modules/ot-oc-template-react-email-compiler/utils")
   ).toBe(false);
   expect(regex.test("node_modules/underscore")).toBe(false);
+  expect(regex.test("node_modules/@ot-react-ui/timeslots")).toBe(false);
 });
