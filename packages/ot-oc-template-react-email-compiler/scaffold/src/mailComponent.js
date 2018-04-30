f6acfbb53871c41412e9a92784ae89cbb7ffeacd
Nick Balestra <nick@balestra.ch>
2018-04-30 12:35:05 +0100

    scaffolds

--- packages/ot-oc-template-react-email-compiler/scaffold/src/mailComponent.js
+++ packages/ot-oc-template-react-email-compiler/scaffold/src/mailComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./styles.css";
 
-const App = props => (
-  <div style={styles.special}>
+const MailComponent = props => (
+  <div style={styles[".special"]}>
     <h1>Hello {props.name}</h1>
   </div>
 );
 
-export default App;
+export default MailComponent;
