16548f208e3a4576162225fd9aacb9e8d2edd3f9
Nick Balestra <nick@balestra.ch>
2018-04-30 12:28:19 +0100

    viewTemplate to gracefull support client-side rendering

--- packages/ot-oc-template-react-email-compiler/lib/viewTemplate.js
+++ packages/ot-oc-template-react-email-compiler/lib/viewTemplate.js
@@ -1,5 +1,30 @@
-const viewTemplate = `function(model){
-  return model.__html ? model.__html : '';
+const viewTemplate = ({
+  reactRoot,
+  externals,
+  bundleHash,
+  bundleName
+}) => `function(model){
+  var modelHTML =  model.__html ? model.__html : '';
+  var staticPath = model.reactComponent.props._staticPath;
+  var props = JSON.stringify(model.reactComponent.props);
+  return modelHTML ? modelHTML : '<div id="${reactRoot}" class="${reactRoot}">' + modelHTML + '</div>' +
+    '<script>' +
+    'window.oc = window.oc || {};' +
+    'oc.cmd = oc.cmd || [];' +
+    'oc.cmd.push(function(oc){' +
+      'oc.requireSeries(${JSON.stringify(externals)}, function(){' +
+        'oc.require(' +
+          '["oc", "reactComponents", "${bundleHash}"],' +
+          '"' + staticPath + '${bundleName}.js",' +
+          'function(ReactComponent){' +
+            'var targetNode = document.getElementById("${reactRoot}");' +
+            'targetNode.setAttribute("id","");' +
+            'ReactDOM.render(React.createElement(ReactComponent,' +  props + '),targetNode);' +
+          '}' +
+        ');' +
+      '});' +
+    '});' +
+  '</script>'
 }`;
 
 module.exports = viewTemplate;
